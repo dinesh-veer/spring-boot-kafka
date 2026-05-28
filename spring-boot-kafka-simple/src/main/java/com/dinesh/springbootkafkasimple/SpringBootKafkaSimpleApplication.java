a5a78caa61605bcecfd0bf21895edc94cd64db27
dinesh-veer <dveer123@hotmail.com>
2026-05-28 10:57:35 +0530

    adding Simple kafka spring boot example skeleton

---
+++ spring-boot-kafka-simple/src/main/java/com/dinesh/springbootkafkasimple/SpringBootKafkaSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.springbootkafkasimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaSimpleApplication.class, args);
+    }
+
+}
---
+++ spring-boot-kafka-simple/src/test/java/com/dinesh/springbootkafkasimple/SpringBootKafkaSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.springbootkafkasimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
