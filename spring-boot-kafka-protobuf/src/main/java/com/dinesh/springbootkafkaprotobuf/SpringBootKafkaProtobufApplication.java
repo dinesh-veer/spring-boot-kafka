767e5c4ba1275ea28a62fdabd53b54b59a7dd3d1
dinesh-veer <dveer123@hotmail.com>
2026-06-10 15:22:24 +0530

    adding code spring boot protobuf

---
+++ spring-boot-kafka-protobuf/src/main/java/com/dinesh/springbootkafkaprotobuf/SpringBootKafkaProtobufApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.springbootkafkaprotobuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProtobufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaProtobufApplication.class, args);
+	}
+
+}
---
+++ spring-boot-kafka-protobuf/src/test/java/com/dinesh/springbootkafkaprotobuf/SpringBootKafkaProtobufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.springbootkafkaprotobuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaProtobufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
