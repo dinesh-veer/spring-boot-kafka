35acddd1f0bd1517ba32fb9a48287fbe030cbe28
dinesh-veer <dveer123@hotmail.com>
2026-06-18 20:07:51 +0530

    adding code spring boot protobuf

---
+++ spring-boot-kafka-protobuf/src/main/java/com/dinesh/springbootkafkaprotobuf/dto/OrderDTO.java
@@ -0,0 +1,10 @@
+package com.dinesh.springbootkafkaprotobuf.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderDTO {
+    public String orderId;
+    public String product;
+    public int quantity;
+}
