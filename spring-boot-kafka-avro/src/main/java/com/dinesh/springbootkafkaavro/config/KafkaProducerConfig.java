9ce36c40841bd70414c5e8e44cea4b7b3eaaf0cd
dinesh-veer <dveer123@hotmail.com>
2026-07-06 08:26:38 +0530

    adding files for spring boot avro

---
+++ spring-boot-kafka-avro/src/main/java/com/dinesh/springbootkafkaavro/config/KafkaProducerConfig.java
@@ -0,0 +1,56 @@
+package com.dinesh.springbootkafkaavro.config;
+
+import com.dinesh.springbootkafkaavro.model.OrderMessage;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
+
+    // --- 1. STRING PRODUCER ---
+    @Bean
+    public ProducerFactory<String, String> stringProducerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean(name = "stringKafkaTemplate")
+    public KafkaTemplate<String, String> stringKafkaTemplate() {
+        return new KafkaTemplate<>(stringProducerFactory());
+    }
+
+    // --- 2. AVRO PRODUCER ---
+    @Bean
+    public ProducerFactory<String, OrderMessage> avroProducerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put("schema.registry.url", schemaRegistryUrl);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean(name = "avroKafkaTemplate")
+    public KafkaTemplate<String, OrderMessage> avroKafkaTemplate() {
+        return new KafkaTemplate<>(avroProducerFactory());
+    }
+}
